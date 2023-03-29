2bc633b8c03b892222cad360ed3557689ba1558c
Elkanah <elkee2003@yahoo.com>
2023-03-29 14:39:52 +0100

    Sign in page with aws done

--- App.tsx
+++ App.tsx
@@ -9,12 +9,18 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import {StatusBar,PermissionsAndroid, Platform} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
+import { withAuthenticator } from 'aws-amplify-react-native';
+
 
 // This code below was meant to be added to 'App.js' if geolocation api needed to be aligned with browser (cross platform apps), or wanted it to support backward compaptibility. However it gave an error line under 'navigator', I assume it is because it is 'App.tsx' not 'App.js'. I had to leave it there, because it seems it works on the app despite the error line and if it is removed my app will not be able to see 'current location'. In other words it's instrumental to my app.
 navigator.geolocation = require('@react-native-community/geolocation');
 
 import RootNavigator from './src/Navigation/Root';
-import Home from './src/Navigation/Home';
+
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 
 const App = ()=>{
@@ -60,4 +66,4 @@ const App = ()=>{
 }
  
 
-export default App;
+export default withAuthenticator(App);
